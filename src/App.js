db300dc1619dced790e895ecb9c288e515f47001
Rasmus Fogelberg <fogelbergrasmus@gmail.com>
2021-12-15 11:12:05 +0100

    Add counter and buttons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
+import { useState } from "react";
+
 function App() {
+
+  // Using state to handle a counter
+  const [count, setCount] = useState(0);
+
+  // Increasing setCount by 1 for every click
+  const clickHandler = () => {
+    setCount(count + 1)
+  }
+
+  // Setting counter to 0
+  const resetHandler = () => {
+    setCount(0)
+  }
+
   return (
-    <div>Hello World</div>
+    <div>
+      <div className="text">Hello World</div>
+      <div>You have clicked the button {count} times!</div>
+      <button onClick={clickHandler}>Click me!</button>
+      <button onClick={resetHandler}>Reset clicks</button>
+    </div>
   );
 }
 
